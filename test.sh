95e8b6dc4842a145508709c53b48895438491150
Paul Walkington <paulwalkington@hotmail.co.uk>
2025-04-04 15:02:44 +0100

    stuff

--- test.sh
+++ test.sh
@@ -24,3 +24,7 @@ echo $json | jq '.'
 
 
 echo "$(echo "$json" | jq -r '.name')"
+
+json2="{n "alternate_contacts": {n "operations": {n "email_address": "aws-hosting-devbox-operations@test-and-trace.nhs.uk",n "name": "AWS Alternate Contacts - devbox - Operations",n "phone_number": "+442083277777",n "title": "."n },n "security": {n "email_address": "aws-hosting-devbox-security@test-and-trace.nhs.uk",n "name": "AWS Alternate Contacts - devbox - Security",n "phone_number": "+442083277777",n "title": "dev"n }n },n "auto_shutdown_enabled": true,n "avm_aws_account_alias": "halo-np-devbox-nl3",n "avm_aws_account_email": "halo-np+ptest-1@test-and-trace.nhs.uk",n "avm_aws_account_name": "ptest-1",n "avm_org_ou_name": "foo",n "avm_subdomain": {n "children": [n "ptest-1"n ],n "parent": "halo-np.org.uk"n },n "environment": "ptest-1",n "roles": "Administrator",n "vpc-config": {n "requestor": "cvm-burendo",n "vpc-size": "Medium",n "vpc-type": "ukhsa"n }n}"
+
+echo $json2 | jq '.'
