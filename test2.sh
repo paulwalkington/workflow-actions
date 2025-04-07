3a3e227ece6b60125ccd1d4ae8eb0618a28626e6
Paul Walkington <paulwalkington@hotmail.co.uk>
2025-04-07 11:39:50 +0100

    stuff

--- test2.sh
+++ test2.sh
@@ -1,19 +1,12 @@
-          jsonString=$( jq -n \
-                  --arg environment "hello" \
-                  --arg avm_aws_account_name "world" \
-                  --arg avm_aws_account_email "from paul" \
-                  --arg avm_aws_account_alias "today" \
-                  --arg avm_org_ou_name "weather" \
-                  '{environment: $environment, avm_aws_account_name: $avm_aws_account_name, avm_aws_account_email: $avm_aws_account_email,
-                   avm_aws_account_alias: $avm_aws_account_alias, avm_org_ou_name: $avm_org_ou_name}' )
+ encodedJson="eyAiZW52aXJvbm1lbnQiOiAicHRlc3QtMSIsICJhdm1fYXdzX2FjY291bnRfbmFtZSI6ICJwdGVz
+  dC0xIiwgImF2bV9hd3NfYWNjb3VudF9lbWFpbCI6ICJoYWxvLW5wK3B0ZXN0LTFAdGVzdC1hbmQt
+  dHJhY2UubmhzLnVrIiwgImF2bV9hd3NfYWNjb3VudF9hbGlhcyI6ICJoYWxvLW5wLWRldmJveC1u
+  bDMiLCAiYXZtX29yZ19vdV9uYW1lIjogImZvbyIgfQo="
+echo $encodedJson 
 
-          echo "---------jsonString---------"
-          echo $jsonString
-          echo "---------jsonString---------"
-
-          # encodedJson=echo $jsonString | base64
-          encodedJson=$(echo $jsonString | base64)
-
-          echo "---------encoded---------"
-          echo $encodedJson
-          echo "---------encoded---------"
+jsonWithoutSpace=$(echo -n $encodedJson)
+echo $jsonWithoutSpace 
+  
+json=$(echo -n $jsonWithoutSpace | base64 -d)
+  
+echo "$json"
