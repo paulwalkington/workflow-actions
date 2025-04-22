2712ad31b4c75958e37691ecedb79f871043a7d3
Paul Walkington <paulwalkington@hotmail.co.uk>
2025-04-22 11:23:32 +0100

    cvm-101 add optional parameter to github workflow to allow bypassing of approval step

--- test6.sh
+++ test6.sh
@@ -1,8 +1,14 @@
-a="12"
+a=""
 
 c="false"
-c=$([ -n "$a" ] && echo "true" || echo "false")
+c=$([ -n "$a" ] && echo true || echo false)
 
 # c=$([ $a=="" ]; echo "false" || echo "true")
 
 echo $c
+
+if [ $c==true ]; then
+    echo "It was set to true"
+else
+    echo "It was set to false"
+fi
