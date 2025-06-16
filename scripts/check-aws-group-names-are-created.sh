3df3c42dc3dbabd3419199c5e1a6c0af583a8dbc
Paul Walkington <paulwalkington@hotmail.co.uk>
2025-06-16 14:59:01 +0100

    cvm-119 - add new workflow to add new workload entry to correct tfvars file

--- scripts/check-aws-group-names-are-created.sh
+++ scripts/check-aws-group-names-are-created.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-function haveAwsGroupNameBeenCreated () {
+function hasAwsGroupNameBeenCreated () {
     groupName=$1
     # groupName="Grp.Aws.Console.Foo.prod.18.Administrator1"
     waitTimeInSeconds=2
@@ -8,6 +8,8 @@ function haveAwsGroupNameBeenCreated () {
 
     totalTimeWaitedInSeconds=0
     groupFound=false
+
+    echo "hello"
 
     echo "Checking for group with groupName [$groupName]"
 
@@ -26,33 +28,45 @@ function haveAwsGroupNameBeenCreated () {
 
         groupFound=true
 
+
         sleep $waitTimeInSeconds
         totalTimeWaitedInSeconds=$((totalTimeWaitedInSeconds + waitTimeInSeconds))
     done
 
-    if $groupFound; then
-        echo "Group found: $groupName"
+
+    if [ "$groupFound" = true ]; then
+        echo "Group with groupName [$groupName] found"
         exit 0
     else
-        echo "Group not found: $groupName"
+        echo "Group with groupName [$groupName] not found after waiting $totalTimeWaitedInSeconds seconds"
         exit 1
     fi
+
+
+
 }
 
 function haveAwsGroupNamesBeenCreated (){
     groupNames=$1
+
+    echo "Checking if AWS group names have been created: $groupNames"
+
     IFS=',' read -ra groupNamesSeparated <<< "$groupNames"
 
     for groupName in "${groupNamesSeparated[@]}"; do
-        haveAwsGroupNameBeenCreated "$groupName"
+        groupFound=$(hasAwsGroupNameBeenCreated "$groupName")
+
         if [ $? -ne 0 ]; then
             echo "Group $groupName not found."
             exit 1
+        else
+            echo "Group $groupName found."
         fi
     done
 
     echo "All groups found successfully."
-    exit 0
 
 }
+
+haveAwsGroupNamesBeenCreated "someName,bill"
 
