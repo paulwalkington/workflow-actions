301fd9c44df5b9f34bedcb7c611a1c83eca8f350
Paul Walkington <paulwalkington@hotmail.co.uk>
2025-06-16 12:25:35 +0100

    cvm-119 - add new workflow to add new workload entry to correct tfvars file

--- scripts/create-aws-group-names.sh
+++ scripts/create-aws-group-names.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
-source ./scripts/create-aws-group-name.sh
+function createAwsAccountGroupName () {
+    workloadShortName=$1
+    environmentType=$2
+    environmentName=$3
+    role=$4
 
+    workloadShortNameLength=${#workloadShortName}
+
+    workloadShortNameUpper=$(echo "${workloadShortName:0:1}" | tr '[:lower:]' '[:upper:]')
+    workloadShortNameLower=$(echo "${workloadShortName:1:$workloadShortNameLength}" | tr '[:upper:]' '[:lower:]')
+    workloadShortNameFormatted=$workloadShortNameUpper$workloadShortNameLower
+
+    environment="${environmentType}"
+
+    if [ -n "$environmentName" ]; then
+        environment="$environmentType.$environmentName"
+    fi
+
+    echo "Grp.Aws.Console.$workloadShortNameFormatted.$environment.$role"
+}
 
 function createAwsAccountGroupNames () {
     workloadShortName=$1
