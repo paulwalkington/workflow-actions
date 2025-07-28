111e74ff0ecefa005f1fdf5d5dffa887321dc91a
Paul Walkington <paulwalkington@hotmail.co.uk>
2025-07-28 17:30:18 +0100

    rename pat token secret in github workflows

--- test10.sh
+++ test10.sh
@@ -7,14 +7,15 @@ if [[ ! "$vend_instance" =~ ^[a-z0-9]{0,10}$ ]]; then
     exit 1
 fi
 
-vend_workload_tags="Department:departmentName,Team:teamName,Support:supportLevel,DataClassification:dataClassification"  
+vend_workload_tags="Department:departmentName,Team:team  Name,Support:supportLevel,DataClassification:dataClassification"  
 # vend_workload_tags="Department:Engineering,Team:DevOps,Support:High,DataClassification:Confidential"
 
 
 # [[:space:]]
 # if [[ ! "$vend_workload_tags" =~ ^Department:[a-zA-Z_0-9]+,Team:[a-zA-Z_0-9-_+:\/]+,Support:[a-zA-Z]+,DataClassification:[a-zA-Z]+$ ]]; then
-if [[ ! "$vend_workload_tags" =~ ^Department:[a-zA-Z_0-9]+,Team:[a-zA-Z0-9_-+:\/]+,Support:[a-zA-Z]+,DataClassification:[a-zA-Z]+$ ]]; then
+if [[ ! "$vend_workload_tags" =~ ^Department:[a-zA-Z_0-9]+,Team:[a-zA-Z0-9_-+:\ \/]+,Support:[a-zA-Z]+,DataClassification:[a-zA-Z]+$ ]]; then
     echo "Error: vend_workload_tags does not match the required format."
     exit 1
 fi
 
+echo "it worked"
