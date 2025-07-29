5af6376cb49c10dab777b16cbbe8b42265a58464
Paul Walkington <paulwalkington@hotmail.co.uk>
2025-07-29 14:50:52 +0100

    cvm-158 - validate string inputs for vending_build_and_deploy_v2.yml

--- test10.sh
+++ test10.sh
@@ -7,7 +7,7 @@ if [[ ! "$vend_instance" =~ ^[a-z0-9]{0,10}$ ]]; then
     exit 1
 fi
 
-vend_workload_tags="Department:departmentName,Team:team  Name,Support:supportLevel,DataClassification:dataClassification"  
+vend_workload_tags="Department:department&Name,Team:team  Name,Support:supportLevel,DataClassification:dataClassification"  
 # vend_workload_tags="Department:Engineering,Team:DevOps,Support:High,DataClassification:Confidential"
 
 
