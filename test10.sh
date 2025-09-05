bb902719e495058e82ae495f553c7d690e0e73e8
Paul Walkington <paulwalkington@hotmail.co.uk>
2025-09-05 11:06:28 +0100

    environment-type needs ot be lower case

--- test10.sh
+++ test10.sh
@@ -7,13 +7,18 @@ if [[ ! "$vend_instance" =~ ^[a-z0-9]{0,10}$ ]]; then
     exit 1
 fi
 
-vend_workload_tags="Department:department&Name,Team:team  Name,Support:supportLevel,DataClassification:dataClassification"  
+# vend_workload_tags="Department:department&Name,Team:team  Name,Support:supportLevel,DataClassification:dataClassification"  
+vend_workload_tags="Department:L2: Science,Team:HPC and Hosting,Support:gold,DataClassification:official"  
 # vend_workload_tags="Department:Engineering,Team:DevOps,Support:High,DataClassification:Confidential"
 
 
-# [[:space:]]
-# if [[ ! "$vend_workload_tags" =~ ^Department:[a-zA-Z_0-9]+,Team:[a-zA-Z_0-9-_+:\/]+,Support:[a-zA-Z]+,DataClassification:[a-zA-Z]+$ ]]; then
-if [[ ! "$vend_workload_tags" =~ ^Department:[a-zA-Z_0-9]+,Team:[a-zA-Z0-9_-+:\ \/()]+,Support:[a-zA-Z]+,DataClassification:[a-zA-Z]+$ ]]; then
+
+# if [[ ! "$vend_workload_tags" =~ ^Department:[a-zA-Z_0-9]+,Team:[a-zA-Z0-9_-+:\ \/()]+,Support:[a-zA-Z]+,DataClassification:[a-zA-Z]+$ ]]; then
+#     echo "Error: vend_workload_tags does not match the required format."
+#     exit 1
+# fi
+
+if [[ ! "$vend_workload_tags" =~ ^Department:[a-zA-Z_0-9:\ ]+,Team:[a-zA-Z0-9_-+:\ \/()]+,Support:[a-zA-Z]+,DataClassification:[a-zA-Z]+$ ]]; then
     echo "Error: vend_workload_tags does not match the required format."
     exit 1
 fi
